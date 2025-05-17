5817a178d32a1ba3337d23191c2ba7fa116c2de8
hyeonju <shj040201@naver.com>
2025-05-18 00:13:40 +0900

    [feat] 걸음수 측정 로직 추가

---
+++ 36-SOPKATHON-iOS-TEAM-3/36-SOPKATHON-iOS-TEAM-3/Presentation/Home/View/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  36-SOPKATHON-iOS-TEAM-3
+//
+//  Created by 성현주 on 5/17/25.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+
+final class HomeView: BaseUIView {
+
+    // MARK: - UI Components
+
+    let walkLabel = UILabel().then{
+        $0.text = "걸음 횟수: 0"
+        $0.font = .boldSystemFont(ofSize: 28)
+        $0.textColor = .black
+        $0.textAlignment = .center
+    }
+
+    //MARK: - Custom Methods
+
+    override func setUI() {
+        addSubviews(walkLabel)
+    }
+
+    override func setLayout() {
+        walkLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+
+}
